bfaf1201a42c789a0d00e690a5a0e27e4988d34f
Jalilovuz <163140718+Jalilovuz@users.noreply.github.com>
2025-09-30 15:38:43 +0500

    tests.py faylida o'zgarishlar

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.137.76']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
